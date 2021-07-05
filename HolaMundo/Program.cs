eb78c653f9daf027b23ba40a81a725135c1031fa
Dostin98 <djp.cruz@yavirac.edu.ec>
2021-07-05 10:36:11 -0500

    Hola Mundo

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dividir();
+            Console.Read();
+        }
+
+        public static void Dividir()
+        {
+            int num1 = 5;
+            int num2 = 3;
+            int num3;
+
+            //Opreadores Unarios
+
+            num3 = -num1;
+            Console.WriteLine("El valor negativo de num1 es {0}", num3);
+            Console.Read();
+        }
+    }
+}
