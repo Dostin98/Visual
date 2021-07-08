896866e4cb9714939dadcb91cd7ab54d3b5614af
Dostin98 <djp.cruz@yavirac.edu.ec>
2021-07-07 23:41:24 -0500

    linq
    ejemplo deber linq

---
+++ LINQ/Pesonaje.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LINQ
+{
+    class Pesonaje
+    { 
+        public int Numero { get; set; }
+        public string Nombre { get; set; }
+
+        public Pesonaje(int Numero, string Nombre)
+        {
+            this.Numero = Numero;
+            this.Nombre = Nombre;
+        }
+
+        public string Imprime()
+        {
+            return Numero + " " + Nombre + "\n";
+        }
+    }
+}
---
+++ LINQ/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pesonaje[] lst =
+            {
+                new Pesonaje(1,"patito"),
+                new Pesonaje(666, "diablito lara"),
+                new Pesonaje(45, "juanito"),
+                new Pesonaje(4, "pepito"),
+                new Pesonaje(67, "carlitos"),
+                new Pesonaje(8, "jualiana"),
+                new Pesonaje(98, "foca"),
+            };
+
+            var personajes = (from d in lst
+                              orderby d.Numero
+                              select d); 
+
+            foreach (var oElement in personajes)
+            {
+                Console.Write(oElement.Imprime());
+            }
+        }
+    }
+}
